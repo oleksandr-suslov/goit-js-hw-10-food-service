15cbf57785264535bb83c7a0a24b922296339f5c
oleksandr-suslov <76252100+oleksandr-suslov@users.noreply.github.com>
2021-06-09 23:18:34 +0300

    add markup menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import './sass/main.scss';
-
+import recipeCard from './templates/card-recipe.hbs'
 import menu from './menu.json';
 
+const menuContainer = document.querySelector('.js-menu');
+// const cardsMarkup = createRecipeCardsMarkup(menu);
+
+menuContainer.insertAdjacentHTML('beforeend', recipeCard(menu));
+
+// function createRecipeCardsMarkup(menu) {
+//     return menu.map(recipeCard).join('');
+// }
