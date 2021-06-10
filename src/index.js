6fc5f49fd2a9ded65e1d4130965c46e43a207011
oleksandr-suslov <76252100+oleksandr-suslov@users.noreply.github.com>
2021-06-10 14:30:23 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -2,11 +2,38 @@ import './sass/main.scss';
 import recipeCard from './templates/card-recipe.hbs'
 import menu from './menu.json';
 
-const menuContainer = document.querySelector('.js-menu');
-// const cardsMarkup = createRecipeCardsMarkup(menu);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-menuContainer.insertAdjacentHTML('beforeend', recipeCard(menu));
+const refs = {
+  menuContainer: document.querySelector('.js-menu'),
+  switch: document.getElementById('theme-switch-toggle'),
+  body: document.querySelector('body'),
+}
 
-// function createRecipeCardsMarkup(menu) {
-//     return menu.map(recipeCard).join('');
-// }
+refs.menuContainer.insertAdjacentHTML('beforeend', recipeCard(menu));
+refs.body.classList.add(Theme.LIGHT);
+refs.switch.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    if (refs.switch.checked) {
+    refs.body.classList.add(Theme.DARK)
+    localStorage.setItem('theme', Theme.DARK);
+    } else {
+    refs.body.classList.remove(Theme.DARK);
+    localStorage.removeItem('theme');
+    }
+}
+
+function getSaveTheme() {
+    if (localStorage.getItem('theme') !== null) {
+        refs.body.classList.add(Theme.DARK);
+        refs.switch.setAttribute('checked', true);
+    }
+    else {
+        refs.body.classList.remove(Theme.DARK);
+    }
+}
+  getSaveTheme()
